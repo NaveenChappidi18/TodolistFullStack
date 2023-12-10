d8787487d46685e5b9f1d8dd8f04790216fe1322
Akhilachappidi <chappidinaveen18102001@gmail.com>
2023-12-10 18:40:05 +0530

    pending changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import List from './List';
 
 function App() {
-  let arr = [];
-  const [todos, UpdateTodos] = useState(arr);
-
-  const AddTodo = () => {
-    let input = document.querySelector('input');
-    let value = input.value;
-    UpdateTodos([...todos, [value,false]]);
-    input.value = "";
-    // console.log(todos);
+  const [currentTodo, setCurrentTodo] = useState([]);
+  
+  const UpdateTodos = (val) => {
+    setCurrentTodo([val, false]);
   }
 
-  const updateTodo = (arr) => {
-    UpdateTodos(arr);
-    
-  }
+  // const AddTodo = () => {
+  //   let input = document.querySelector('input');
+  //   let value = input.value;
+  //   UpdateTodos([...todos, [value,false]]);
+  //   input.value = "";
+  //   // console.log(todos);
+  // }
+  useEffect(()=>{
+    const fetchData = async() => {
+      await fetch("http://localhost:3000/putList",
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/ json'
+          },
+          body: {
+            "email": "kasasunil344@gmail.com",
+            "todo":  currentTodo 
+          }
+        }
+      )
+    }
+    fetchData();
+  },[currentTodo])
+
+
   
   return (
     <div>
       <div className="card">
-        <input type="text" placeholder='Enter your work' />
-        <button onClick={AddTodo}>Add</button>
+        <input type="text" placeholder='Enter your work' id="work" />
+        <button onClick={() => { UpdateTodos(document.querySelector('#work').value) }}>Add</button>
       </div>
-      <List children={todos} func={updateTodo}/>
+      <List />
     </div>
     
   );
--- src/List.js
+++ src/List.js
@@ -1,54 +1,59 @@
-function List({ children, func }) {
+import React, { useEffect, useState } from 'react';
+function List() {
+    const email = "kasasunil344@gmail.com";
+    const [change, setChange] = useState(0);
+    let arr = [];
+    const [totData, setTotData] = useState(arr);
+    useEffect(() => {
+        const data = fetch(`http://localhost:3000/list/${email}`).then((res) => res.json());
+        data.then((res) => {
+            setTotData(res);
+            console.log(res,totData);
+        });
+    }, [change]);
     const clear = (ind) => {
-        const updatedChildren = children.filter((_, index) => index !== ind);
-        console.log(updatedChildren);
-        func(updatedChildren);
+        fetch(`http://localhost:3000/delete/${ind}/${email}`, {
+            method: "DELETE",
+            header: {
+                "content-Type": "application/JSON",
+            }
+        });
+        const c = 1 - change;
+        setChange(c);
     }
 
-    const edit = (val, ind) => {
-        let arr = [];
-        for (let i = 0; i < children.length; i++) {
-            if (i == ind) {
-                if (val.length > 0)
-                    arr.push([val, false]);
-                else
-                    arr.push([children[i][0], false]);
-            }
-            else {
-                arr.push(children[i]);
-            }
-        }
-        func(arr);
-    }
-    const updateArray = (ind) => {
-        let arr = [];
-        for (let i = 0; i < children.length; i++) {
-            if (i == ind) {
-                arr.push([children[i][0], true]);
-            }
-            else {
-                arr.push(children[i]);
-            }
-        }
-        func(arr);
-    }
+    // const edit = (val, ind) => {
+        
+    // }
+    // const updateArray = (ind) => {
+    //     let arr = [];
+    //     for (let i = 0; i < children.length; i++) {
+    //         if (i == ind) {
+    //             arr.push([children[i][0], true]);
+    //         }
+    //         else {
+    //             arr.push(children[i]);
+    //         }
+    //     }
+    //     func(arr);
+    // }
     return (
         <div>
-            {children.map((ele, index) => {
-                if (ele[1]) {
+            {totData.map((ele, index) => {
+                if (ele.isOk) {
                     return (
                         <span key={index}>
                             <br />
-                            <input type="text" id="edit" placeholder={ele[0]} />
-                            <button onClick={() => edit(document.getElementById("edit").value,index)}>ok</button>
+                            <input type="text" id="edit" placeholder={ele.data} />
+                            {/* <button onClick={() => edit(document.getElementById("edit").value,index)}>ok</button> */}
                         </span>
                     );
                 } else {
                     return (
                         <span key={index}>
-                            <li>{ele[0]}</li>
+                            <li>{ele.data}</li>
                             <button onClick={() => clear(index)}>complete</button>
-                            <button onClick={() => updateArray(index)}>edit</button>
+                            {/* <button onClick={() => updateArray(index)}>edit</button> */}
                         </span>
                     );
                 }
